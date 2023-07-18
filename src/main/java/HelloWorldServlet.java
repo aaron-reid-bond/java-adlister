d7a3c6772fec7d4b0eb0f7adb8ed290e25a94479
Aaron Bond <aaron.reid.bond@gmail.com>
2023-07-18 09:57:58 -0700

    servlet counter

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,24 @@
-import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import  javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    private int hitCount;
 
+    public void init() {
+        // Reset hit counter.
+        hitCount = 0;
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        hitCount++;
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        String title = "Total Number of Hits: ";
+        out.printf("<h1>Hello, World! %s%d</h1>", title, hitCount);
     }
 }
